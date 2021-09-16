b88e27bcd946c71c96a5653d0112b520693fb7a1
Nathan-Cahill <87508548+Nathan-Cahill@users.noreply.github.com>
2021-09-16 11:45:59 +1000

    Added all compenents for the browser

---
+++ dbConnect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://alexcloud:alexcloud@nsw-bot.nkfb6.mongodb.net/deakincrowds?retryWrites=true&w=majority";
+let mongoClient = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+
+let projectsCollection;
+
+mongoClient.connect((err,db) => {
+    // projectsCollection = mongoClient.db("deakinCrowds").collection("projects");
+     if(!err){
+       console.log('Database Connected')
+     }else{
+       console.log('[error]',err)
+     }
+ });
+ 
+
+exports.mongoClient = mongoClient;
+
+const dbConnect = ()=>{
+    mongoClient.connect((err,db) => {
+        projectsCollection = mongoClient.db("deakinCrowds").collection("projects");
+        if(!err){
+          console.log('Database Connected')
+        }else{
+          console.log('[error]',err)
+        }
+    });
+    
+}
+
+
+ 
